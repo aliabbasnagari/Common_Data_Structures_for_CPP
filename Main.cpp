6ad3bfe55765cfb076682ed0c5f08fe083929c2d
Ali Abbas <70366131+aliabbasnagari@users.noreply.github.com>
2022-12-10 16:23:39 +0500

    fixed merge

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,9 @@ Adnan Hayat
 Muhammad Wissam - 21I-0709
 */
 
-
+#include<chrono>
+#include<thread>
+#include<iostream>
 #include "AVL.h"
 #include "BTree.h"
 #include "Utils.h"
@@ -14,10 +16,6 @@ Muhammad Wissam - 21I-0709
 #include "Utils.h"
 #include "Menus.h"
 #include "Interface.h"
-
-#include<chrono>
-#include<thread>
-#include<iostream>
 using namespace std;
 
 int main() 
@@ -43,46 +41,6 @@ int main()
 	cout << endl;
 	cout << endl;
 	cout << endl;
-=======
-int main() {
-	string path = "datafiles/test2.csv";
-	Queue<Entry>* q;
-	q= readCSV(path);
-	AVLTree<DataNode> avl;
-	AVLTree<DataNode> avlname;
-	BTree<DataNode> btree(4);
-	avl.key = "ID";
-	avlname.key = "STATE";
-	btree.key = "ID";
-	int len = q->Size();
-	for (int i = 0; i < len; i++)
-	{
-		DataNode node;
-		node.filepath = path;
-		node.id = q->Front().getId();
-		node.line = (i + 1);
-		btree.insert(node);
-		avl.insert(node);
-		node.filepath = path;
-		node.id = int_of_str(q->Front().state);
-		node.line = (i + 1);
-		avlname.insert(node);
-		q->dequeue();
-	}
-	cout << endl << endl;
-	// avl.LevelOrderTreversal(avl.root);
-	cout << "Range Search: \n";
-	//avl.getQueryrange("get <year,state,deaths> where # id=15-20");
-	cout << "Single Search: \n";
-	avl.getQuery("get <state> where # id=18");
-	cout << "\nString Search: \n";
-	avlname.getQuery("get <id> where # state=Indiana");
-	
-
-	//Testing for B-Tree
-	cout << endl << "B-Tree: " << endl;
-	btree.traverse();
-
 
 	return 0;
 }
